9ac8e0a868ba345528559586fd55d265f1fa6449
alexrem1 <remidy@live.co.uk>
2021-04-07 23:08:36 +0100

    Connecting to Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore ";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByY9gDGN62IxXfsleWTzSizyQt6wCQqjE",
+  authDomain: "practice-7531a.firebaseapp.com",
+  projectId: "practice-7531a",
+  storageBucket: "practice-7531a.appspot.com",
+  messagingSenderId: "946965221276",
+  appId: "1:946965221276:web:4f6381cc178cee37134259",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.firebase.auth();
+firebase.firestore().settings({ experimentalForceLongPolling: true });
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectAuth, projectFirestore, timestamp };
