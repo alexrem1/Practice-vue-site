cbdbc1b2fa03f1edec7a5baae9d530d2b32d8673
alexrem1 <remidy@live.co.uk>
2021-04-11 23:42:55 +0100

    Creating a signup view

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-center h-100">
+      <form @submit.prevent="handleSubmit" class="padding-login">
+        <div class="mb-3">
+          <label class="form-label">Email address</label>
+          <input type="email" class="form-control" required v-model="email" />
+          <div class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            required
+            v-model="password"
+          />
+        </div>
+        <div class="error" v-if="error">{{ error }}</div>
+        <button class="btn mt-2">Log in</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import useLogin from "@/composables/useLogin";
+import { ref } from "vue";
+export default {
+  setup() {
+    const { error, login } = useLogin(email, password);
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("User logged in");
+      }
+    };
+
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 85vh;
+}
+
+.padding-login {
+  padding-top: 12%;
+}
+</style>
