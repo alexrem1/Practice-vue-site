caf73903383da14b0ab2443a2bd5aeeb4ee6bc45
alexrem1 <remidy@live.co.uk>
2021-04-17 23:03:56 +0100

    Getting Real Time Enquiry Data

--- src/views/Account/Account.vue
+++ src/views/Account/Account.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="container">
     <h1>My account</h1>
+    <div v-if="error" class="error">Could not fetch your enquiries</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">
+        <div class="d-flex flex-row justify-content-evenly p-5 ">
+          {{ doc.subject }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import getCollection from "@/composables/getCollection";
+
+export default {
+  setup() {
+    const { error, documents } = getCollection("enquiries");
+
+    return { error, documents };
+  },
+};
 </script>
 
 <style scoped>
