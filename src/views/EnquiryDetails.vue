c24339462cf0d615e5d91e923ae191480449b87a
alexrem1 <remidy@live.co.uk>
2021-04-19 15:14:09 +0100

    Enquiry details template

--- src/views/EnquiryDetails.vue
+++ src/views/EnquiryDetails.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="container">
-    <h2>Enquiry details. ID - {{ id }}</h2>
-  </div></template
->
+  <div v-if="error">{{ error }}</div>
+  <div v-if="enquiry">
+    <!-- playlist information -->
+    <div class="container">
+      <img :src="enquiry.coverUrl" />
+
+      <h2>{{ enquiry.subject }}</h2>
+      <p>Created by: {{ enquiry.userName }}</p>
+      <p>{{ enquiry.description }}</p>
+      <p>Created at {{ enquiry.createdAt }}</p>
+      more to come
+    </div>
+  </div>
+</template>
 
 <script>
+import getDocument from "../composables/getDocument";
+
 export default {
   props: ["id"],
+  setup(props) {
+    const { error, document: enquiry } = getDocument("enquiries", props.id);
+
+    return { error, enquiry };
+  },
 };
 </script>
 
