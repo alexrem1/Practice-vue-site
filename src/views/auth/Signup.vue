9a4eefdf417954b3ee2d70074241422fcc2ddcd7
alexrem1 <remidy@live.co.uk>
2021-04-28 19:28:12 +0100

    Added more user details to the sign up form

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -10,6 +10,7 @@
             required
             v-model="displayName"
           />
+
           <label class="form-label mt-3">Email address</label>
           <input type="email" class="form-control" required v-model="email" />
           <div class="form-text">
@@ -24,6 +25,55 @@
           />
           <div class="form-text">
             Your password must be 8-20 characters long.
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-8">
+            <br />
+            <input
+              class="m-1"
+              type="text"
+              placeholder="First name"
+              v-model="firstName"
+              required
+            />
+            <input
+              class="m-1"
+              type="text"
+              placeholder="Last name"
+              v-model="lastName"
+              required
+            />
+            <input
+              class="m-1"
+              type="text"
+              placeholder="House no. and name"
+              v-model="firstLine"
+              required
+            />
+            <input
+              class="m-1"
+              type="text"
+              placeholder="Street name"
+              v-model="street"
+              required
+            />
+            <br />
+            <input
+              class="m-1"
+              type="text"
+              placeholder="Postcode"
+              v-model="postcode"
+              required
+            />
+            <input
+              class="m-1"
+              type="number"
+              maxlength="11"
+              placeholder="Phone Number"
+              v-model="phoneNumber"
+              required
+            />
           </div>
         </div>
         <div class="error" v-if="error">{{ error }}</div>
@@ -50,25 +100,59 @@
 import useSignup from "@/composables/useSignup";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import useCollection from "@/composables/useCollection";
+import getUser from "@/composables/getUser";
+import { timestamp } from "@/firebase/config";
 
 export default {
   setup() {
     const { error, signup, isPending } = useSignup();
+    const { addDoc } = useCollection("userDetails");
+    const { user } = getUser();
 
     const email = ref("");
     const password = ref("");
     const displayName = ref("");
     const router = useRouter();
+    const firstName = ref("");
+    const lastName = ref("");
+    const firstLine = ref("");
+    const street = ref("");
+    const postcode = ref("");
+    const phoneNumber = ref("");
 
     const handleSubmit = async () => {
       const res = await signup(email.value, password.value, displayName.value);
+      await addDoc({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        firstLine: firstLine.value,
+        street: street.value,
+        postcode: postcode.value,
+        createdAt: timestamp(),
+        userId: user.value.uid,
+        phoneNumber: phoneNumber.value,
+      });
       if (!error.value) {
         console.log("User logged in");
         router.push({ name: "Home" });
       }
     };
 
-    return { email, password, displayName, handleSubmit, error, isPending };
+    return {
+      email,
+      password,
+      displayName,
+      handleSubmit,
+      error,
+      isPending,
+      firstName,
+      lastName,
+      firstLine,
+      street,
+      postcode,
+      phoneNumber,
+    };
   },
 };
 </script>
