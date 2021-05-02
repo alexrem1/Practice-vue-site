198ed46cd4785100705702f35f4fe830f724c75b
alexrem1 <remidy@live.co.uk>
2021-05-02 17:50:38 +0100

    Chat window and date formatting

--- src/views/Enquiries/EnquiryDetails.vue
+++ src/views/Enquiries/EnquiryDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="error">{{ error }}</div>
+  <div v-if="error" class="error">{{ error }}</div>
   <div v-if="enquiry">
     <div class="container">
       <img :src="enquiry.coverUrl" />
@@ -12,6 +12,16 @@
       <button v-if="ownership" @click="handleDelete">Delete Enquiry</button>
     </div>
   </div>
+  <div class="container">
+    <ChatWindow />
+    <form>
+      <textarea
+        placeholder="Type a message and hit enter to send"
+        v-model="message"
+        @keypress.enter.prevent="handleSubmit"
+      ></textarea>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -19,17 +29,23 @@ import useStorage from "@/composables/useStorage";
 import useDocument from "@/composables/useDocument";
 import getDocument from "@/composables/getDocument";
 import getUser from "@/composables/getUser";
-import { computed } from "vue";
+import useCollection from "@/composables/useCollection";
+import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
+import { timestamp } from "../../firebase/config";
+import ChatWindow from "../../components/ChatWindow.vue";
 
 export default {
+  components: { ChatWindow },
   props: ["id"],
   setup(props) {
     const { error, document: enquiry } = getDocument("enquiries", props.id);
     const { user } = getUser();
     const { deleteDoc } = useDocument("enquiries", props.id);
     const { deleteImage } = useStorage();
+    const { addDoc } = useCollection("support-chat");
     const router = useRouter();
+    const message = ref("");
 
     const handleDelete = async () => {
       await deleteImage(enquiry.value.filePath);
@@ -43,7 +59,29 @@ export default {
       );
     });
 
-    return { error, enquiry, ownership, handleDelete };
+    const handleSubmit = async () => {
+      const chat = {
+        message: message.value,
+        user: user.value.displayName,
+        createdAt: timestamp(),
+        userId: user.value.uid,
+      };
+      await addDoc(chat);
+      if (!error.value) {
+        message.value = "";
+      } else {
+        error.value = "You do not own this enquiry.";
+      }
+    };
+
+    return {
+      error,
+      enquiry,
+      ownership,
+      handleDelete,
+      handleSubmit,
+      message,
+    };
   },
 };
 </script>
@@ -51,5 +89,19 @@ export default {
 <style scoped>
 .container {
   min-height: 85vh;
+}
+form {
+  margin: 10px;
+}
+textarea {
+  width: 50%;
+  max-width: 50%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
 }
 </style>
